4aff298a6fd2bce5834f938d5cd0f710c81cdf32
zengnianmei <1976919119@qq.com>
2017-09-04 10:32:46 +0800

    优化

--- deploy/server100/bin/stop.sh
+++ deploy/server100/bin/stop.sh
@@ -2,4 +2,7 @@
 export CATALINA_BASE=catalina_base
 
 ${CATALINA_HOME}/bin/shutdown.sh -config ${CATALINA_BASE}/conf/server.xml
-ps -aef | grep java|grep ${CATALINA_BASE}| grep -v grep | sed 's/ [ ]*/:/g' |cut -d: -f2|kill -9 `cat`
+DOWNPORT=`ps -aef | grep java|grep ${CATALINA_BASE}| grep -v grep | sed 's/ [ ]*/:/g' |cut -d: -f2`
+if [ -z ${DOWNPORT} ];then
+    kill -9 ${DOWNPORT}
+fi
